9baa723bbc79130a17f7cba4abf5ba0d395fdaad
OlhaFedorets <fedoretsolha@gmail.com>
2025-02-17 17:39:59 +0300

    change app

--- src/app/AppHttpRequests.tsx
+++ src/app/AppHttpRequests.tsx
@@ -2,61 +2,130 @@ import {type ChangeEvent, type CSSProperties, useEffect, useState} from 'react'
 import Checkbox from '@mui/material/Checkbox'
 import {CreateItemForm} from '@/common/components/CreateItemForm/CreateItemForm'
 import {EditableSpan} from '@/common/components/EditableSpan/EditableSpan'
+import axios from "axios";
+
+export type Todolist = {
+    "id": string
+    "title": string
+    "addedDate": string
+    "order": number
+}
+
+export type FieldError = {
+    error: string
+    field: string
+}
+
+export type CreateTodoResponse = {
+    data: { item: Todolist }
+    fieldsErrors: FieldError[]
+    messages: string[]
+    resultCode: number
+}
+
+export type DeleteTodoResponse = {
+    data: {}
+    fieldsErrors: FieldError[]
+    messages: string[]
+    resultCode: number
+}
+
+const token = 'e8132478-0685-4d8d-8c23-e73d7660c77e'
+const apiKey = 'e96b23ce-5bf9-4df9-9d8f-68d7ac71e75d'
 
 export const AppHttpRequests = () => {
-  const [todolists, setTodolists] = useState<any>([])
-  const [tasks, setTasks] = useState<any>({})
+    const [todolists, setTodolists] = useState<Todolist[]>([])
+    const [tasks, setTasks] = useState<any>({})
 
-  useEffect(() => {
-    // get todolists
-  }, [])
+    useEffect(() => {
+        axios.get<Todolist[]>('https://social-network.samuraijs.com/api/1.1/todo-lists', {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        }).then(res => setTodolists(res.data))
+    }, [])
 
-  const createTodolist = (title: string) => {}
+    const createTodolist = (title: string) => {
+        axios.post<CreateTodoResponse>('https://social-network.samuraijs.com/api/1.1/todo-lists', {title}, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'API-KEY': apiKey
+            }
+        }).then(res => {
+            const todolist = res.data.data.item
+            setTodolists([todolist, ...todolists])
+        })
+    }
 
-  const deleteTodolist = (id: string) => {}
+    const deleteTodolist = (id: string) => {
+        axios.delete<DeleteTodoResponse>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'API-KEY': apiKey
+            }
+        }).then(() => {
+            setTodolists(todolists.filter(todolist => todolist.id !== id))
+        })
+    }
 
-  const changeTodolistTitle = (id: string, title: string) => {}
+    const changeTodolistTitle = (id: string, title: string) => {
+        axios.put<any>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${id}`, {title}, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'API-KEY': apiKey
+            }
+        }).then((res) => {
+            console.log(res.data)
+            setTodolists(todolists.map(todolist => todolist.id === id ? {...todolist, title} : todolist))
+        })
+    }
 
-  const createTask = (todolistId: string, title: string) => {}
+    const createTask = (todolistId: string, title: string) => {
+    }
 
-  const deleteTask = (todolistId: string, taskId: string) => {}
+    const deleteTask = (todolistId: string, taskId: string) => {
+    }
 
-  const changeTaskStatus = (e: ChangeEvent<HTMLInputElement>, task: any) => {}
+    const changeTaskStatus = (e: ChangeEvent<HTMLInputElement>, task: any) => {
+    }
 
-  const changeTaskTitle = (task: any, title: string) => {}
+    const changeTaskTitle = (task: any, title: string) => {
+    }
 
-  return (
-      <div style={{margin: '20px'}}>
-        <CreateItemForm onCreateItem={createTodolist}/>
-        {todolists.map((todolist: any) => (
-            <div key={todolist.id} style={container}>
-              <div>
-                <EditableSpan value={todolist.title}
-                              onChange={title => changeTodolistTitle(todolist.id, title)}/>
-                <button onClick={() => deleteTodolist(todolist.id)}>x</button>
-              </div>
-              <CreateItemForm onCreateItem={title => createTask(todolist.id, title)}/>
-              {tasks[todolist.id]?.map((task: any) => (
-                  <div key={task.id}>
-                    <Checkbox checked={task.isDone}
-                              onChange={e => changeTaskStatus(e, task)}/>
-                    <EditableSpan value={task.title}
-                                  onChange={title => changeTaskTitle(task, title)}/>
-                    <button onClick={() => deleteTask(todolist.id, task.id)}>x</button>
-                  </div>
-              ))}
-            </div>
-        ))}
-      </div>
-  )
+    return (
+        <div style={{margin: '20px'}}>
+            <CreateItemForm onCreateItem={createTodolist}/>
+            {todolists.map(todolist => (
+                <div key={todolist.id} style={container}>
+                    <div>
+                        <EditableSpan value={todolist.title}
+                                      onChange={title => changeTodolistTitle(todolist.id, title)}/>
+                        <button onClick={() => deleteTodolist(todolist.id)}>x</button>
+                    </div>
+                    <CreateItemForm onCreateItem={title => createTask(todolist.id, title)}/>
+                    {tasks[todolist.id]?.map((task: any) => (
+                        <div key={task.id}>
+                            <Checkbox checked={task.isDone}
+                                      onChange={e => changeTaskStatus(e, task)}/>
+                            <EditableSpan value={task.title}
+                                          onChange={title => changeTaskTitle(task, title)}/>
+                            <button onClick={() => deleteTask(todolist.id, task.id)}>x</button>
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </div>
+    )
 }
 
 const container: CSSProperties = {
-  border: '1px solid black',
-  margin: '20px 0',
-  padding: '10px',
-  width: '300px',
-  display: 'flex',
-  justifyContent: 'space-between',
-  flexDirection: 'column',
+    border: '1px solid black',
+    margin: '20px 0',
+    padding: '10px',
+    width: '300px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'column',
 }
+
+
